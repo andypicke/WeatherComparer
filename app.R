25c83f8a43d9318fcac231e81506896edef4fd4c
andypicke <andypicke@gmail.com>
2017-08-07 09:39:25 -0600

    fix typo in label

--- app.R
+++ app.R
@@ -43,7 +43,7 @@ ui <- fluidPage(
                         numericInput("year1","Compare from year:",2015,1970,2017,step=1),
                         numericInput("year2","To year:",2016,1970,2017,step=1),
                         numericInput("month1","Between month:",1,12,1,step=1),
-                        numericInput("month2","And month month:",1,12,2,step=1),
+                        numericInput("month2","And month:",1,12,2,step=1),
                         textInput("stcode","At Airport Station Code:",value = "KDEN"),
                         actionButton("button", "Execute!")
                 ),
