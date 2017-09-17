e7c2e6d90247fb085ff801aae487a697d74481f5
andypicke <andypicke@gmail.com>
2017-09-17 10:44:35 -0600

    update

--- scripts.R
+++ scripts.R
@@ -27,13 +27,35 @@ get_weather_cleaned <- function(st_code){
         wea
 }
 
+get_weather_cleaned_oneyear <- function(st_code,the_year,month_start,month_end){
+        url_base <- 'https://s3-us-west-2.amazonaws.com/wundergrounddaily/cleaned/'
+        s3_url <- paste0(url_base, st_code, '_cleaned.csv')
+        wea<-read.csv(s3_url)
+        wea$date <- lubridate::ymd(wea$date)
+        wea <- wea %>% filter(year==the_year) %>% 
+                filter(month>=month_start & month<=month_end)
+        wea
+}        
+
+
+get_all_years <- function(year1,year2,st_code,month_start,month_end){
+        url_base <- 'https://s3-us-west-2.amazonaws.com/wundergrounddaily/cleaned/'
+        s3_url <- paste0(url_base, st_code, '_cleaned.csv')
+        wea<-read.csv(s3_url)
+        wea$date <- lubridate::ymd(wea$date)
+        wea <- wea %>% 
+                filter(year>=year1 & year<=year2) %>% 
+                filter(month>=month_start & month<=month_end)
+        wea
+}        
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # function to return data for only 'current' year
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-get_current_year <- function(wea){
+get_current_year <- function(wea,the_year){
         wea_current <- wea %>%
-                filter(year=input$the_year)
+                filter(year==the_year)
         wea_current
 }
 
@@ -43,11 +65,12 @@ get_current_year <- function(wea){
 # compute daily averages and standard deviation over all years
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 get_avg_temps <- function(wea){
-        wea %>% 
+        wea_avg <- wea %>% 
                 group_by(yday) %>% 
                 summarise(tavg=mean(mean_temp,na.rm=TRUE),
                           sd_low=tavg-sd(mean_temp,na.rm=TRUE),
-                          sd_igh=tavg+sd(mean_temp,na.r=TRUE)) 
+                          sd_high=tavg+sd(mean_temp,na.r=TRUE)) 
+        wea_avg
 }
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
